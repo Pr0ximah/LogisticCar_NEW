fbbaf528355d02ad5c157c84276ae46591945735
Pr0ximah <51467731+Pr0ximah@users.noreply.github.com>
2024-03-19 22:09:28 +0800

    merge: dev_IMU

--- lib/Definition/PinDef.h
+++ lib/Definition/PinDef.h
@@ -1,6 +1,9 @@
 #ifndef _PIN_DEF_H
 #define _PIN_DEF_H
 
+int tx_IMU = 6; // IMUtx 接入 软串口rx
+int rx_IMU = 5; // IMUrx 接入 软串口tx
+
 // 电机端口
 const int port_motor_FL = 11;  // 左前电机PWM
 const int port_motor_FR = 10;  // 右前电机PWM
--- lib/IMU/IMU.h
+++ lib/IMU/IMU.h
@@ -1,4 +1,16 @@
 #ifndef _IMU_H
 #define _IMU_H
 
+/// @brief heading初始值，缺省401
+static double heading_original = 401;
+
+/// @brief 初始化IMU，启动软串口
+void IMU_Init();
+
+/// @brief 获取IMU当前朝向，(-180, 180] (deg)
+double IMU_Heading();
+
+/// @brief 打印十六进制数字如0xff
+void print_0x_num(int16_t *arr, int len) {
+
 #endif  //_IMU_H
