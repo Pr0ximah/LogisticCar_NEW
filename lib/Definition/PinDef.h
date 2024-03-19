d22e43b80529f7936c0799e307d68d1067ba8eed
Pr0ximah <51467731+Pr0ximah@users.noreply.github.com>
2024-03-19 22:06:39 +0800

    封装IMU

--- lib/Definition/PinDef.h
+++ lib/Definition/PinDef.h
@@ -1,4 +1,7 @@
 #ifndef _PIN_DEF_H
 #define _PIN_DEF_H
 
+int tx_IMU = 6; // IMUtx 接入 软串口rx
+int rx_IMU = 5; // IMUrx 接入 软串口tx
+
 #endif//_PIN_DEF_H
--- lib/IMU/IMU.cpp
+++ lib/IMU/IMU.cpp
@@ -1,1 +1,48 @@
 #include "IMU.h"
+
+#include <SoftwareSerial.h>
+#include "PinDef.h"
+
+// 软串口 (rx, tx)
+SoftwareSerial SoftSerial(tx_IMU, rx_IMU);
+
+void IMU_Init() { SoftSerial.begin(9600); }
+
+double IMU_Heading() {
+    int16_t Buffer[9];
+    bool valid_flag = False;
+    while (!valid_flag) {
+        if (SoftSerial.find(0x55)) {
+            for (int i = 0; i < 9; i++) {
+                while (!SoftSerial.available()) {
+                    delayMicroseconds(1);
+                }
+                Buffer[i] = SoftSerial.read();
+            }
+            if (Buffer[0] == 0x55 && Buffer[1] == 0x01 && Buffer[2] == 0x06) {
+                double heading = (double((int16_t)(Buffer[8] << 8) | Buffer[7])) / 32768.0 * 180;
+                if (heading_original == 401) {
+                    heading_original = heading;
+                }
+                heading -= heading_original;
+                // 归一化到 (-180 deg ~ 180 deg]
+                while (heading > 180) {
+                    heading -= 360;
+                }
+                while (heading <= -180) {
+                    heading += 360;
+                }
+                return heading;
+            }
+        }
+        delayMicroseconds(1);
+    }
+}
+
+void print_0x_num(int16_t *arr, int len) {
+    for (int i = 0; i < len; i++) {
+        Serial.print(arr[i], HEX);
+        Serial.print(" ");
+    }
+    Serial.print("\n");
+}
