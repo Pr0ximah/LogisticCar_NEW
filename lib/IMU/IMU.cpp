3cdfe58576054c2fa231062d291d13ad23e9fb4b
520AWEI <1941773493@qq.com>
2024-03-21 18:11:55 +0800

    IMU6050 restart

--- lib/IMU/IMU.cpp
+++ lib/IMU/IMU.cpp
@@ -5,49 +5,31 @@
 
 #include "ConstDef.h"
 #include "PinDef.h"
-
 // 软串口 (rx, tx)
-SoftwareSerial SoftSerial(tx_IMU, rx_IMU);
-static double heading_original = 401;  // heading初始值，缺省401
-
-void IMU_Init() { SoftSerial.begin(9600); }
-
-double IMU_Heading() {
-    int16_t buffer[9];
-    unsigned long long time_start = millis();
-    while (true) {
-        if (millis() - time_start > IMU_JUMP_TIME) {
-            return 0;
-        }
-        if (SoftSerial.find(0x55)) {
-            for (int i = 0; i < 9; i++) {
-                while (!SoftSerial.available()) {
-                    delayMicroseconds(1);
-                }
-                buffer[i] = SoftSerial.read();
-            }
-            if (buffer[0] == 0x55 && buffer[1] == 0x01 && buffer[2] == 0x06) {
-                for (int i = 0; i < 9; i++) {
-                    Serial.print(buffer[i], HEX);
-                    Serial.print(" ");
-                }
-                Serial.print("\n");
-
-                double heading = (float((int16_t)(buffer[8] << 8) | buffer[7])) / 32768.0 * 180;
-                // if (heading_original == 401) {
-                //     heading_original = heading;
-                // }
-                // heading -= heading_original;
-                // // 归一化到 (-180 deg ~ 180 deg]
-                // while (heading > 180) {
-                //     heading -= 360;
-                // }
-                // while (heading <= -180) {
-                //     heading += 360;
-                // }
-                return heading;
-            }
-        }
-        delayMicroseconds(1);
+void MyIMU::InitIMU(){
+    double angleSpeed_sum = 0;       //初始化静态偏移量和变量
+    int16_t angleSpeedOri;  // 角加速度原始数值
+    int16_t temp1, temp2, temp3, temp4, temp5;  // 不使用这些变量，仅用于imu函数传参
+    Serial.println("IMU initializing starts...");
+    delay(1500);                    //等待车辆静止
+    for (int i = 0; i < 500; i++) {
+        imu.getMotion6(&temp1, &temp2, &temp3, &temp4, &temp5, &angleSpeedOri); //获取三轴加速度和三轴角速度初始值
+        float angleSpeed0 = angleSpeedOri / coe;
+        angleSpeed_sum += angleSpeed0;
+        delay(10);
     }
+    angleSpeed_bias = angleSpeed_sum / 500;
+    Serial.println("IMU initialize finished!");
+}
+
+void MyIMU::updateIMU(){
+        int16_t angleSpeedOri;  // 角加速度原始数值
+        int16_t temp1, temp2, temp3, temp4, temp5;  // 不使用这些变量，仅用于imu函数传参
+        imu.getMotion6(&temp1, &temp2, &temp3, &temp4, &temp5, &angleSpeedOri); //获取三轴加速度和三轴角速度初始值
+        angleSpeed = angleSpeedOri / coe - angleSpeed_bias;
+        float angleAcce;
+        curTime = millis();
+        double imuUpdateTimeInterval = updateTimeCur - updateTimeLast;
+        lastTime=curTime;
+        angle += angleAcce * imuUpdateTimeInterval / 1000;
 }
--- lib/IMU/IMU.h
+++ lib/IMU/IMU.h
@@ -1,11 +1,36 @@
 #ifndef _IMU_H
 #define _IMU_H
 
+#include <MPU6050.h>
+class myIMU{
+private:
+    MPU6050 imu;
+    double curTime=0;
+    double lastTime=0;
+    double angleSpeed;
+    double angle;
+    double angleSpeed_bias;
+    const double coe=65.5;
+public:
+    myIMU(){
+        angleSpeed=0;
+        angle =0;
+        angleSpeed_bias=0;
+    }
+    void resetIMU(){
+        angle=0;
+        angleSpeed=0;
+    }
+    void setIMU(double ANGLE){
+        angle=ANGLE;
+    }
+    void InitIMU();
+    void updateIMU();
+    double ReadAngleSpeed();
+    double getAngle(){
+        return angle;
+    }
 
-/// @brief 初始化IMU，启动软串口
-void IMU_Init();
 
-/// @brief 获取IMU当前朝向，(-180, 180] (deg)
-double IMU_Heading();
-
+};
 #endif  //_IMU_H
